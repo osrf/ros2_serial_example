0e3a7c4d93f9b749151d5aa0efc2844f9f3a3f49
Chris Lalancette <clalancette@openrobotics.org>
2019-02-05 15:13:42 -0500

    Add in signal handling.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ros2_serializer.cpp
+++ ros2_serializer.cpp
@@ -1,3 +1,4 @@
+#include <csignal>
 #include <iostream>
 #include <memory>
 
@@ -5,11 +6,28 @@
 
 #include "ros2_serial_transport.hpp"
 
+volatile sig_atomic_t running = 1;
+
+void signal_handler(int signum)
+{
+  (void)signum;
+  running = 0;
+}
+
 int main(int argc, char *argv[])
 {
   std::unique_ptr<Transport_node> transport_node = std::make_unique<UART_node>("/dev/ttyACM0", B115200, 0);
 
-  transport_node->init();
+  signal(SIGINT, signal_handler);
+
+  if (transport_node->init() < 0) {
+    return 1;
+  }
+
+  while (running) {
+  }
+
+  transport_node->close();
 
   return 0;
 }
