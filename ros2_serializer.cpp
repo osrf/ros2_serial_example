439cbafbbcc941e8b5f5612682c1977a52f1e903
Chris Lalancette <clalancette@openrobotics.org>
2019-02-05 15:20:22 -0500

    Call read on the data.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ros2_serializer.cpp
+++ ros2_serializer.cpp
@@ -3,8 +3,11 @@
 #include <memory>
 
 #include <termios.h>
+#include <unistd.h>
 
 #include "ros2_serial_transport.hpp"
+
+constexpr int BUFFER_SIZE = 1024;
 
 volatile sig_atomic_t running = 1;
 
@@ -16,15 +19,22 @@ void signal_handler(int signum)
 
 int main(int argc, char *argv[])
 {
+  uint8_t topic_ID = 255;
+  int length = 0;
+  char data_buffer[BUFFER_SIZE] = {};
+
   std::unique_ptr<Transport_node> transport_node = std::make_unique<UART_node>("/dev/ttyACM0", B115200, 0);
 
-  signal(SIGINT, signal_handler);
+  ::signal(SIGINT, signal_handler);
 
   if (transport_node->init() < 0) {
     return 1;
   }
 
   while (running) {
+    while ((length = transport_node->read(&topic_ID, data_buffer, BUFFER_SIZE)) > 0) {
+    }
+    ::usleep(1);
   }
 
   transport_node->close();
