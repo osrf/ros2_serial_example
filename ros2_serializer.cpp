b353e0e667bbb624722383d4f957f615843bbd54
Chris Lalancette <clalancette@openrobotics.org>
2019-02-08 15:47:15 -0500

    Add the ability to specify the serial device.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- ros2_serializer.cpp
+++ ros2_serializer.cpp
@@ -18,12 +18,43 @@ void signal_handler(int signum)
   running = 0;
 }
 
+static void usage(const char *name)
+{
+  printf("Usage: %s [options]\n\n"
+         "  -d <device> UART device. Default /dev/ttyACM0\n"
+         "  -h          Print this help message\n",
+         name);
+}
+
 int main(int argc, char *argv[])
 {
-  (void)argc;
-  (void)argv;
+  char device[64] = "/dev/ttyACM0";
 
-  std::unique_ptr<Transport_node> transport_node = std::make_unique<UART_node>("/dev/ttyACM0", B115200, 0);
+  int ch;
+  while ((ch = getopt(argc, argv, "d:h")) != EOF) {
+    switch (ch)
+    {
+      case 'd':
+        if (optarg != nullptr) {
+          strcpy(device, optarg);
+        }
+        break;
+    case 'h':
+      usage(argv[0]);
+      return 0;
+    default:
+      usage(argv[0]);
+      return 1;
+    }
+  }
+
+  if (optind < argc)
+  {
+    usage(argv[0]);
+    return 1;
+  }
+
+  std::unique_ptr<Transport_node> transport_node = std::make_unique<UART_node>(device, B115200, 0);
 
   ::signal(SIGINT, signal_handler);
 
