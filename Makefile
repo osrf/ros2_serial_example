66c76deb5dfa4c8956d4abc7149c03e92a71c2f9
Chris Lalancette <clalancette@openrobotics.org>
2019-02-08 16:46:23 -0500

    Start adding in a writer.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- Makefile
+++ Makefile
@@ -3,10 +3,13 @@ CXXFLAGS=-g -Wall -Wextra -std=c++14 -I install/include
 LIBS=-Linstall/lib -lfastcdr -lfastrtps
 SERIALIZER_OBJS=ros2_serializer.o ros2_serial_transport.o RtpsTopics.o battery_status_PubSubTypes.o battery_status_Publisher.o battery_status_.o
 
-all: ros2_serializer
+all: ros2_serializer ros2_serial_write
 
 ros2_serializer: $(SERIALIZER_OBJS)
 	$(CXX) $(CXXFLAGS) -o $@ $(SERIALIZER_OBJS) $(LIBS)
+
+ros2_serial_write: ros2_serial_write.o ros2_serial_transport.o
+	$(CXX) $(CXXFLAGS) -o $@ ros2_serial_write.o ros2_serial_transport.o
 
 clean:
 	rm -f ros2_serializer *.o *~
--- ros2_serial_write.cpp
+++ ros2_serial_write.cpp
@@ -1,24 +1,11 @@
-#include <csignal>
 #include <cstdio>
 #include <cstring>
-#include <iostream>
 #include <memory>
 
 #include <termios.h>
 #include <unistd.h>
 
 #include "ros2_serial_transport.hpp"
-#include "RtpsTopics.hpp"
-
-constexpr int BUFFER_SIZE = 1024;
-
-volatile sig_atomic_t running = 1;
-
-void signal_handler(int signum)
-{
-  (void)signum;
-  running = 0;
-}
 
 static void usage(const char *name)
 {
@@ -58,32 +45,11 @@ int main(int argc, char *argv[])
 
   std::unique_ptr<Transport_node> transport_node = std::make_unique<UART_node>(device, B115200, 0);
 
-  ::signal(SIGINT, signal_handler);
-
   if (transport_node->init() < 0) {
     return 1;
   }
 
   ::sleep(1);
-
-  char data_buffer[BUFFER_SIZE] = {};
-  int length = 0;
-  uint8_t topic_ID = 255;
-
-  RtpsTopics topics;
-
-  topics.init();
-
-  running = 1;
-
-  while (running) {
-    while ((length = transport_node->read(&topic_ID, data_buffer, BUFFER_SIZE)) > 0) {
-      topics.publish(topic_ID, data_buffer, sizeof(data_buffer));
-    }
-    ::usleep(1);
-  }
-
-  transport_node->close();
 
   return 0;
 }
