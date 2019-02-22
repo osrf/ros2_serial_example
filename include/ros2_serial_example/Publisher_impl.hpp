e72a2b05e871e0fe3a3c2ecd7c3e421a563b322f
Chris Lalancette <clalancette@openrobotics.org>
2019-02-22 15:41:08 -0500

    Switch to a templatized Publisher_impl.
    No need for a class-per-type now.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- include/ros2_serial_example/Publisher_impl.hpp
+++ include/ros2_serial_example/Publisher_impl.hpp
@@ -5,7 +5,6 @@
 #include <string>
 
 #include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
 #include <std_msgs/msg/string__rosidl_typesupport_fastrtps_cpp.hpp>
 
 #include <fastcdr/Cdr.h>
@@ -13,12 +12,13 @@
 
 #include "ros2_serial_example/publisher.hpp"
 
-class std_msgs_String_Publisher : public Publisher
+template<typename T>
+class Publisher_impl : public Publisher
 {
 public:
-    explicit std_msgs_String_Publisher(const std::shared_ptr<rclcpp::Node> & node, const std::string & name)
+    explicit Publisher_impl(const std::shared_ptr<rclcpp::Node> & node, const std::string & name)
     {
-        pub = node->create_publisher<std_msgs::msg::String>(name);
+        pub = node->create_publisher<T>(name);
     }
 
     void dispatch(char data_buffer[], ssize_t length) override
@@ -26,11 +26,11 @@ public:
         // string topic
         eprosima::fastcdr::FastBuffer cdrbuffer(data_buffer, length);
         eprosima::fastcdr::Cdr cdrdes(cdrbuffer);
-        auto msg = std::make_shared<std_msgs::msg::String>();
+        auto msg = std::make_shared<T>();
         std_msgs::msg::typesupport_fastrtps_cpp::cdr_deserialize(cdrdes, *(msg.get()));
         pub->publish(msg);
     }
 
 private:
-    std::shared_ptr<rclcpp::Publisher<std_msgs::msg::String>> pub;
+    std::shared_ptr<rclcpp::Publisher<T>> pub;
 };
