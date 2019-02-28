490becf254c728015e73ef87a227747d2392d829
Chris Lalancette <clalancette@openrobotics.org>
2019-02-28 14:36:21 -0500

    Make the dummy serial program be an input and output.
    That way we can test both sides of the comms.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- src/dummy_serial.cpp
+++ src/dummy_serial.cpp
@@ -1,6 +1,8 @@
+#include <csignal>
 #include <cstdio>
 #include <cstring>
 #include <memory>
+#include <thread>
 
 #include <termios.h>
 #include <unistd.h>
@@ -19,6 +21,35 @@ static void usage(const char *name)
              "  -d <device> UART device. Default /dev/ttyACM0\n"
              "  -h          Print this help message\n",
              name);
+}
+
+volatile sig_atomic_t running = 1;
+
+static void signal_handler(int signum)
+{
+    (void)signum;
+    running = 0;
+}
+
+void read_thread_func(Transporter * transporter)
+{
+    char data_buffer[BUFFER_SIZE] = {};
+    ssize_t length = 0;
+    topic_id_size_t topic_ID;
+
+    while (running)
+    {
+        // Process data coming over serial
+        while ((length = transporter->read(&topic_ID, data_buffer, BUFFER_SIZE)) > 0)
+        {
+            fprintf(stderr, "Topic ID: %d, data: ", topic_ID);
+            for (ssize_t i = 0; i < length; ++i)
+            {
+                fprintf(stderr, "0x%x ", data_buffer[i]);
+            }
+            fprintf(stderr, "\n");
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -51,26 +82,38 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    std::unique_ptr<Transporter> transporter = std::make_unique<UARTTransporter>(device, B115200, 0);
+    std::unique_ptr<Transporter> transporter = std::make_unique<UARTTransporter>(device, B115200, 1);
 
     if (transporter->init() < 0)
     {
         return 1;
     }
 
+    ::signal(SIGINT, signal_handler);
+
+    std::thread read_thread(read_thread_func, transporter.get());
+
     size_t headlen = transporter->get_header_length();
 
-    char data_buffer[BUFFER_SIZE] = {};
-    eprosima::fastcdr::FastBuffer cdrbuffer(&data_buffer[headlen], sizeof(data_buffer) - headlen);
-    eprosima::fastcdr::Cdr scdr(cdrbuffer);
-    scdr << "aa";
-    transporter->write(9, data_buffer, scdr.getSerializedDataLength());
+    while (running)
+    {
+        char data_buffer[BUFFER_SIZE] = {};
+        eprosima::fastcdr::FastBuffer cdrbuffer(&data_buffer[headlen], sizeof(data_buffer) - headlen);
+        eprosima::fastcdr::Cdr scdr(cdrbuffer);
+        scdr << "aa";
+        transporter->write(9, data_buffer, scdr.getSerializedDataLength());
 
-    char data_buffer2[BUFFER_SIZE] = {};
-    eprosima::fastcdr::FastBuffer cdrbuffer2(&data_buffer2[headlen], sizeof(data_buffer2) - headlen);
-    eprosima::fastcdr::Cdr scdr2(cdrbuffer2);
-    scdr2 << "bb";
-    transporter->write(12, data_buffer2, scdr2.getSerializedDataLength());
+        char data_buffer2[BUFFER_SIZE] = {};
+        eprosima::fastcdr::FastBuffer cdrbuffer2(&data_buffer2[headlen], sizeof(data_buffer2) - headlen);
+        eprosima::fastcdr::Cdr scdr2(cdrbuffer2);
+        scdr2 << "bb";
+        transporter->write(12, data_buffer2, scdr2.getSerializedDataLength());
+
+        // every 100 milliseconds
+        ::usleep(100000);
+    }
+
+    read_thread.join();
 
     transporter->close();
 
