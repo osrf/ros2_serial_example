c28b4954b4e1b4457ff45a1c2247b6c8f91eb2ae
Chris Lalancette <clalancette@openrobotics.org>
2019-04-02 10:22:54 -0400

    Initial implementation of UDP transport.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ ros2_serial_example/include/ros2_serial_example/udp_transporter.hpp
@@ -0,0 +1,159 @@
+/****************************************************************************
+ *
+ * Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+// Originally based on:
+// https://github.com/PX4/px4_ros_com/blob/69bdf6e70f3832ff00f2e9e7f17d9394532787d6/templates/microRTPS_transport.h
+// but split out of there.
+
+#ifndef ROS2_SERIAL_EXAMPLE__UDP_TRANSPORTER_HPP_
+#define ROS2_SERIAL_EXAMPLE__UDP_TRANSPORTER_HPP_
+
+// C++ includes
+#include <cstdint>
+#include <string>
+
+#include <poll.h>
+
+// Local includes
+#include "ros2_serial_example/transporter.hpp"
+
+namespace ros2_to_serial_bridge
+{
+
+namespace transport
+{
+
+/**
+ * The UDPTransporter class is an implementation of the abstract Transporter
+ * class specifically for talking to UDP sockets.
+ */
+class UDPTransporter final : public Transporter
+{
+public:
+    /**
+     * Construct a UDPTransporter object with the given UDP parameters.
+     *
+     * @param[in] recv_port The UDP port to receive on.
+     * @param[in] send_port The UDP port to send from.
+     * @param[in] read_poll_ms The amount of time to wait for the read file
+     *                         descriptor to become ready before timing out.
+     *                         Larger numbers mean less CPU time is spent, but
+     *                         also mean that the calls to read block longer.
+     *                         It is recommended to start with 100 milliseconds.
+     * @param[in] ring_buffer_size The number of bytes to allocate to the
+     *                             underlying ring buffer that is used to
+     *                             accept data from the UDP socket.  Larger
+     *                             numbers will allow the transport to accept
+     *                             larger packets (or more of them), at the
+     *                             expense of memory.  It is recommended to
+     *                             start with 8192.
+     */
+    UDPTransporter(const std::string & protocol,
+                   uint16_t recv_port,
+                   uint16_t send_port,
+                   uint32_t read_poll_ms,
+                   size_t ring_buffer_size);
+    ~UDPTransporter() override;
+
+    UDPTransporter(UDPTransporter const &) = delete;
+    UDPTransporter& operator=(UDPTransporter const &) = delete;
+    UDPTransporter(UDPTransporter &&) = delete;
+    UDPTransporter& operator=(UDPTransporter &&) = delete;
+
+    /**
+     * Do UDP specific initialization.
+     *
+     * This method is an override of the one provided by the Transporter class
+     * and does the setup of the underlying UDP socket.
+     *
+     * @returns 0 on success, -1 on error.
+     */
+    int init() override;
+
+    /**
+     * Do UDP specific de-initialization.
+     *
+     * This method is an override of the one provided by the Transporter class
+     * and undoes the steps that the init() method does.
+     *
+     * @returns 0 on success, -1 on error.
+     */
+    int close() override;
+
+private:
+    /**
+     * Read data from the underlying UDP socket and store it in the ring buffer.
+     *
+     * This method is an override of the abstract one in the Transporter class
+     * and attempts to read data from the underlying UDP socket.  If no data
+     * becomes available before the read_timeout_ms set in the constructor, it
+     * returns 0 with no action taken.  If data is available on the UDP socket,
+     * it is read into the ring buffer and the method returns the number of
+     * bytes read.
+     */
+    ssize_t node_read() override;
+
+    /**
+     * Write data to the underlying UDP socket.
+     *
+     * This method is an override of the abstract one in the Transporter class
+     * and attempts to write data to the underlying UDP socket.  This method
+     * will block until all of the data is sent, or until an error occurs.
+     *
+     * @params[in] buffer The buffer containing the data to write.
+     * @params[in] len The number of bytes in the buffer to write.
+     * @returns The number of bytes written on success (which must be equal to
+     *          len), or -1 on error.
+     */
+    ssize_t node_write(void *buffer, size_t len) override;
+
+    /**
+     * Detect whether the UDP sockets are ready to send and receive data.
+     *
+     * @returns true if the underlying UDP sockets are configured and ready,
+     *          false otherwise.
+     */
+    bool fds_OK() override;
+
+    uint16_t recv_port_{0};
+    uint16_t send_port_{0};
+    uint32_t read_poll_ms_{0};
+    int recv_fd_{-1};
+    int send_fd_{-1};
+    uint32_t write_timeout_us_{20};
+    struct pollfd poll_fd_[1] = {};
+};
+
+}  // namespace transport
+}  // namespace ros2_to_serial_bridge
+
+#endif
---
+++ ros2_serial_example/src/udp_transporter.cpp
@@ -0,0 +1,236 @@
+/****************************************************************************
+ *
+ * Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+// Originally based on:
+// https://github.com/PX4/px4_ros_com/blob/69bdf6e70f3832ff00f2e9e7f17d9394532787d6/templates/microRTPS_transport.cpp
+// but modified to split UDP code out of the original transporter.
+
+#include <cerrno>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <unistd.h>
+
+#include "ros2_serial_example/transporter.hpp"
+#include "ros2_serial_example/udp_transporter.hpp"
+
+namespace ros2_to_serial_bridge
+{
+
+namespace transport
+{
+
+UDPTransporter::UDPTransporter(const std::string & protocol,
+                               uint16_t recv_port,
+                               uint16_t send_port,
+                               uint32_t read_poll_ms,
+                               size_t ring_buffer_size):
+    Transporter(protocol, ring_buffer_size),
+    recv_port_(recv_port),
+    send_port_(send_port),
+    read_poll_ms_(read_poll_ms)
+{
+}
+
+UDPTransporter::~UDPTransporter()
+{
+    close();
+}
+
+int UDPTransporter::init()
+{
+    // Setup the receiver
+    struct sockaddr_in receiver_inaddr{};
+
+    // udp socket data
+    receiver_inaddr.sin_family = AF_INET;
+    receiver_inaddr.sin_port = ::htons(recv_port_);
+    receiver_inaddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+
+    if ((recv_fd_ = ::socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        ::fprintf(stderr, "create socket failed: %s\n", ::strerror(errno));
+        return -errno;
+    }
+
+    ::printf("Trying to connect...\n");
+
+    if (::bind(recv_fd_, reinterpret_cast<struct sockaddr *>(&receiver_inaddr),
+               sizeof(receiver_inaddr)) < 0)
+    {
+        ::fprintf(stderr, "bind failed %s\n", ::strerror(errno));
+        return -1;
+    }
+
+    // Now make the incoming socket non-blocking.
+    int flags = ::fcntl(recv_fd_, F_GETFL, 0);
+    if (flags < 0)
+    {
+        ::fprintf(stderr, "Failed to get flags for UDP receive: %s\n",
+                  ::strerror(errno));
+        return -1;
+    }
+    flags |= O_NONBLOCK;
+    if (::fcntl(recv_fd_, F_SETFL, flags) < 0)
+    {
+        ::fprintf(stderr, "Failed to set flags for UDP receive: %s\n",
+                  ::strerror(errno));
+        return -1;
+    }
+
+    poll_fd_[0].fd = recv_fd_;
+    poll_fd_[0].events = POLLIN;
+
+    // Setup the sender
+    if ((send_fd_ = ::socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        ::fprintf(stderr, "create socket failed: %s\n", ::strerror(errno));
+        return -1;
+    }
+
+    struct sockaddr_in sender_outaddr{};
+    sender_outaddr.sin_family = AF_INET;
+    sender_outaddr.sin_port = ::htons(send_port_);
+
+    if (::inet_aton("127.0.0.1", &sender_outaddr.sin_addr) == 0)
+    {
+        ::fprintf(stderr, "inet_aton() failed: %s\n", ::strerror(errno));
+        return -1;
+    }
+
+    return 0;
+}
+
+bool UDPTransporter::fds_OK()
+{
+    return (-1 != recv_fd_ && -1 != send_fd_);
+}
+
+int UDPTransporter::close()
+{
+    if (-1 != recv_fd_)
+    {
+        ::shutdown(recv_fd_, SHUT_RDWR);
+        ::close(recv_fd_);
+        recv_fd_ = -1;
+    }
+
+    if (-1 != send_fd_)
+    {
+        ::shutdown(send_fd_, SHUT_RDWR);
+        ::close(send_fd_);
+        send_fd_ = -1;
+    }
+
+    ::memset(&poll_fd_, 0, sizeof(poll_fd_));
+
+    return 0;
+}
+
+ssize_t UDPTransporter::node_read()
+{
+    if (!fds_OK())
+    {
+        return -1;
+    }
+
+    ssize_t ret = 0;
+    int r = ::poll(reinterpret_cast<struct pollfd *>(poll_fd_), 1, read_poll_ms_);
+
+    if (r == 1 && (poll_fd_[0].revents & POLLIN) != 0)
+    {
+        ret = ringbuf_.read(recv_fd_);
+    }
+
+    return ret;
+}
+
+ssize_t UDPTransporter::node_write(void *buffer, size_t len)
+{
+    if (nullptr == buffer || !fds_OK())
+    {
+        return -1;
+    }
+
+    uint32_t intr_times = 0;
+
+    // Ensure that the entire buffer gets out to the file descriptor (unless a
+    // fatal error occurs)
+    uint8_t *b = static_cast<uint8_t *>(buffer);
+    size_t n = len;
+    while (n > 0)
+    {
+        ssize_t ret = ::write(send_fd_, b, n);
+        if (ret == -1)
+        {
+            if (errno == EINTR || errno == EAGAIN)
+            {
+                // We can get an EINTR/EAGAIN for a temporary failure (for
+                // instance, the remote end of a pipe is temporarily busy and not
+                // draining the data fast enough), or for a more permanent one
+                // (for instance, nothing is draining the remote end of a pipe).
+                // Just doing "continue" here in the latter case means that we'll
+                // be stuck in this loop forever, burning CPU.  Thus, we add a
+                // small delay and a counter in here.  If we get into here
+                // `write_timeout_us` times in a row (with no data being
+                // transferred in between), we presume this is a permanent failure
+                // and return an error to the application.
+                intr_times++;
+                if (intr_times > write_timeout_us_)
+                {
+                    // Too many failures, set an errno and get out.
+                    errno = EBUSY;
+                    break;
+                }
+                ::usleep(1);
+                continue;
+            }
+
+            break;
+        }
+        intr_times = 0;
+        n -= ret;
+        b += ret;
+    }
+
+    return (n > 0) ? -1 : len;
+}
+
+}  // namespace transport
+}  // namespace ros2_to_serial_bridge
